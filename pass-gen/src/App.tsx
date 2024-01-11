f0bf64a2f9e7f32e5711580ea058b2ec4b756abf
sascha <sascha.mauch@endava.com>
2024-01-11 14:12:59 +0100

    changed bg color

--- pass-gen/src/App.tsx
+++ pass-gen/src/App.tsx
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <>
-     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-900 text-orange-500'>
       <h1 className='text-white uppercase text-center my-3 font-bold'>my password generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input 
